364fe549fcd24aa87b38c3a9a825277350476ed5
Aria Groult <aria.groult@outlook.fr>
2018-11-06 15:23:07 +0100

    added eslint & deleted semi-colons

--- client/src/vuex/store.js
+++ client/src/vuex/store.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import challenges from './modules/challenges'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-const debug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-    modules: {
-        challenges,
-    },
-    strict: debug,
+	modules: {
+		challenges,
+	},
+	strict: debug,
 })
