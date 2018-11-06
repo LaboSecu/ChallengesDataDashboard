ac9e3f1576be31ae1c7032aaa2cda27f05928af1
Aria Groult <aria.groult@outlook.fr>
2018-11-06 18:16:25 +0100

    cleaned file

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,5 +1,7 @@
 const challenges = require('./challenges/challenges.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(challenges);
+  app.configure(users);
 };
