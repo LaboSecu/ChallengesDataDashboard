42b4e11876fc1357934771f0fb6492ab2a08e5bb
Aria Groult <aria.groult@outlook.fr>
2018-11-07 11:41:19 +0100

    Added page not found vue

---
+++ client/src/views/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="not-found-container">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+    </div>
+</template>
+
+<style lang="less">
+    #not-found-container{
+        text-align: center;
+        font-family: Arial,sans-serif;
+        margin-top: 100px;
+        padding: 20px;
+        color: #333;
+        h1{
+            font-weight: 100;
+            font-size: 8em;
+            margin-bottom: 10px;
+        }
+        h2{
+            font-size: 2em;
+            font-weight: 100;
+        }
+    }
+</style>
