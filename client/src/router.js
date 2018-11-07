b5b96b3515b576c1f33e2948a844096baed8b4d6
Aria Groult <aria.groult@outlook.fr>
2018-11-07 11:40:53 +0100

    Added 404 route

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import PageNotFound from './views/PageNotFound'
 
 Vue.use(Router)
 
@@ -17,6 +18,10 @@ export default new Router({
 			path: '/dashboard',
 			name: 'dashboard',
 			component: () => import ('./views/Dashboard.vue')
+		},
+		{
+			path: '*',
+			component: PageNotFound
 		}
 	]
 })
