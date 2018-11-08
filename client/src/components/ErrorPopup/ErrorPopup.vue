c4c46adb355bfb3f22452f3d6cb9353c42ff0cf8
Aria Groult <aria.groult@outlook.fr>
2018-11-08 17:42:15 +0100

    Created Error Popup Component

---
+++ client/src/components/ErrorPopup/ErrorPopup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id='error-wrapper'>
+        {{errorMessage}}
+    </div>
+</template>
+
+<script>
+    import './ErrorPopup.less'
+
+    export default {
+        name: 'ErrorPopup',
+        props: [
+            "errorMessage"
+        ]
+    }
+</script>
