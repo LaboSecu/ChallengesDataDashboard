48fe57cee0f1e9214264cd9691f983ef4c6ecb79
Aria Groult <aria.groult@outlook.fr>
2018-11-09 20:40:53 +0100

    added feathers-client authentication

--- client/src/components/Form/Form.vue
+++ client/src/components/Form/Form.vue
@@ -1,103 +1,100 @@
 <template>
-    <div id='form-container'>
-        <div class='input-group'>
-            <input type='text' required placeholder='Email...' v-model='mailInput' @focus='toggleExtensionDiv(1, 0)'
-                   @blur='toggleExtensionDiv(1, 1)'>
-            <LinearGrowDiv v-bind:extended='firstDivExtended'/>
-        </div>
-        <div class='input-group'>
-            <input type='password' required placeholder='Password...' v-model='passwordInput' @focus='toggleExtensionDiv(2, 0)'
-                   @blur='toggleExtensionDiv(2, 1)'>
-            <LinearGrowDiv v-bind:extended='secondDivExtended'/>
-        </div>
-        <div class='input-group input-group-submit'>
-            <button @click='submitLogin'>Submit</button>
-            <img v-if='submitPending' src='tail-spin.svg'/>
-        </div>
-        <ErrorPopup v-if='errorPopupDisplayed' error-message='The Email or Password you entered is incorrect.'/>
+  <div id='form-container'>
+    <div class='input-group'>
+      <input type='text' placeholder='Email...' v-model='mailInput' @focus='toggleExtensionDiv(1, 0)'
+             @blur='toggleExtensionDiv(1, 1)'>
+      <LinearGrowDiv v-bind:extended='firstDivExtended'/>
     </div>
+    <div class='input-group'>
+      <input type='password' placeholder='Password...' v-model='passwordInput'
+             @focus='toggleExtensionDiv(2, 0)'
+             @blur='toggleExtensionDiv(2, 1)'>
+      <LinearGrowDiv v-bind:extended='secondDivExtended'/>
+    </div>
+    <div class='input-group input-group-submit'>
+      <button @click='submitLogin'>Submit</button>
+      <img v-if='submitPending' src='tail-spin.svg'/>
+    </div>
+    <ErrorPopup v-if='errorPopupDisplayed' error-message='The Email or Password you entered is incorrect.'/>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
-    import Router from '../../router'
-    import {setCookie} from '../../cookie-manager';
-    import LinearGrowDiv from '../LinearGrowDiv'
-    import ErrorPopup from '../ErrorPopup/ErrorPopup'
-    import './Form.less'
+  import {client} from '../../feathers-services'
+  import Router from '../../router'
+  import LinearGrowDiv from '../LinearGrowDiv'
+  import ErrorPopup from '../ErrorPopup/ErrorPopup'
+  import './Form.less'
 
-    export default {
-        name: 'LoginForm',
-        components: {
-            LinearGrowDiv,
-            ErrorPopup
-        },
-        data() {
-            return {
-                firstDivExtended: false,
-                secondDivExtended: false,
-                errorPopupDisplayed: false,
-                mailInput: '',
-                passwordInput: '',
-                submitPending: false
+  export default {
+    name: 'LoginForm',
+    components: {
+      LinearGrowDiv,
+      ErrorPopup
+    },
+    data () {
+      return {
+        firstDivExtended: false,
+        secondDivExtended: false,
+        errorPopupDisplayed: false,
+        mailInput: '',
+        passwordInput: '',
+        submitPending: false
+      }
+    },
+    methods: {
+      toggleExtensionDiv: function (number, actionCode) {
+        switch (actionCode) {
+          case 1:
+            switch (number) {
+              case 1:
+                this.firstDivExtended = false
+                break
+              case 2:
+                this.secondDivExtended = false
+                break
+              default:
+                break
             }
-        },
-        methods: {
-            toggleExtensionDiv: function (number, actionCode) {
-                switch (actionCode) {
-                    case 1:
-                        switch (number) {
-                            case 1:
-                                this.firstDivExtended = false
-                                break
-                            case 2:
-                                this.secondDivExtended = false
-                                break
-                            default:
-                                break
-                        }
-                        break
-                    case 0:
-                        switch (number) {
-                            case 1:
-                                this.firstDivExtended = true
-                                break
-                            case 2:
-                                this.secondDivExtended = true
-                                break
-                            default:
-                                break
-                        }
-                        break
-                    default:
-                        break
-                }
-            },
-            submitLogin: function(){
-                let mailInput = this.mailInput
-                let passwordInput = this.passwordInput
-                this.submitPending = true
-                axios.post('/authentication', {
-                    email: mailInput,
-                    password: passwordInput,
-                    strategy: 'local'
-                }).then((response) => {
-                    if(response.status === 201) {
-                        setCookie('access-token', response.data.accessToken, 7)
-                        this.submitPending = false
-                        Router.push('dashboard')
-                    }
-                }).catch(() => {
-                    this.errorPopupDisplayed = true
-                })
+            break
+          case 0:
+            switch (number) {
+              case 1:
+                this.firstDivExtended = true
+                break
+              case 2:
+                this.secondDivExtended = true
+                break
+              default:
+                break
             }
-        },
-        created(){
-            document.addEventListener('keydown', (e) => {
-                if(e.keyCode === 13) this.submitLogin()
-            })
+            break
+          default:
+            break
         }
+      },
+      submitLogin: async function () {
+        let mailInput = this.mailInput
+        let passwordInput = this.passwordInput
+        this.submitPending = true
+        try{
+          const payload = Object.assign({email: mailInput, password: passwordInput}, { strategy: 'local' })
+          await client.authenticate(payload)
+          Router.push('dashboard')
+        } catch (e) {
+          this.mailInput = ''
+          this.passwordInput = ''
+          this.errorPopupDisplayed = true
+          this.submitPending = false
+        }
+      }
+    },
+    created () {
+      document.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) this.submitLogin()
+      })
     }
+  }
 </script>
 
 <style>
--- client/src/feathers-services.js
+++ client/src/feathers-services.js
@@ -1,12 +1,16 @@
 const feathers = require('@feathersjs/feathers')
 const socketio = require('@feathersjs/socketio-client')
+const auth = require('@feathersjs/authentication-client')
 const io = require('socket.io-client')
 
-const socket = io('/')
+const socket = io('http://localhost:3030/')
 const app = feathers()
 
 // Set up Socket.io client with the socket
 app.configure(socketio(socket))
+app.configure(auth({
+	storage: window.localStorage
+}))
 
-// Receive real-time events through Socket.io
+export const client = app
 export const challengesService = app.service('challenges')
